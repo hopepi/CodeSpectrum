8b323fb15967ec10ad2e6dc2e5358b68506566d6
hopepi <umuttozkannis@gmail.com>
2025-09-25 22:05:15 +0300

    config created

---
+++ config.py
@@ -0,0 +1,13 @@
+import torch
+import os
+from pathlib import Path
+
+ROOT_DIR   = Path(__file__).resolve().parent
+DATA_DIR   = ROOT_DIR / "malebin"
+TRAIN_PATH = os.path.join(DATA_DIR , "train")
+TEST_PATH  = os.path.join(DATA_DIR , "test")
+VAL_PATH   = os.path.join(DATA_DIR , "val")
+
+BATCH_SIZE = 32
+SEED = 42
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
